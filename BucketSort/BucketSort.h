83792bdad299c9dd13ac7b5afd7032bfdf385e05
Eddy <Eddy@AMDRYZEN>
2026-02-09 14:39:49 -0600

    a

--- BucketSort/BucketSort.h
+++ BucketSort/BucketSort.h
@@ -8,7 +8,7 @@ class BucketSort {
 		uint64_t* inputBuffer;
 		uint64_t* outputBuffer;
 		uint64_t size;
-		uint64_t* buckets;
+		uint64_t* buckets[][] = new uint64_t[8][1 << 8];
 		void sort(uint64_t* buffer, uint64_t size, int shift, int level);
 	public:	
 		BucketSort(uint64_t * inputBuffer, uint64_t * outputBuffer, uint64_t size);

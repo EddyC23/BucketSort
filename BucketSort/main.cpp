7f26a5e1036fc1a775b4c12b708ec83a061345d9
Eddy <Eddy@DESKTOP-5MO2H7K>
2026-02-16 13:54:53 -0600

    aw

--- BucketSort/main.cpp
+++ BucketSort/main.cpp
@@ -3,7 +3,7 @@
 #include <windows.h>
 #include <random>
 #include "BucketSort.h"
-
+#include <chrono>
 
 
 int main() {
@@ -16,7 +16,11 @@ int main() {
 	for (size_t i = 0; i < 1 << 16; i++) {
 		input[i] = distribution(gen);
 	}
-
+	//flag stops after different levels eg L0 l1 to time benchmark
+	//L0 with vortex S stream 5.3, append only can improve, corner cases
+	//interval tree
+	//flag as parameter
+	//static is ceiling 
 	BucketSort b(input, output, 1 << 16);
 	b.sort();
 	b.printArray(output, 1 << 16);
